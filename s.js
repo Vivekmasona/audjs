7d5531f10dbeeee2b2c78790e44830f5d1e2a074
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2024-05-25 02:05:24 +0530

    Update s.js

--- s.js
+++ s.js
@@ -5,7 +5,7 @@
       "url": "https://github.com/Vivekmasona/audjs/raw/main/audio/rt.mp3"
     },
     {
-      "title": "Song 2",
+      "title": "vivek",
       "url": "https://github.com/Vivekmasona/audjs/raw/main/audio/audio_rt.mp3"
     }
   ]
